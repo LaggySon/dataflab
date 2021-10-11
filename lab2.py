648b8d760b0c12a134ed00d3bd7558d93643c0c5
LaggySon <56988314+LaggySon@users.noreply.github.com>
2021-10-11 14:59:31 -0400

    edited lab2.py

--- lab2.py
+++ lab2.py
@@ -1,35 +1,40 @@
 import pygame
 
-pygame.init()
+def main():
 
-pygame.display.set_caption("Lab 2")
+    pygame.init()
 
-WIDTH = 800
-HEIGHT = 480
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Lab 2")
 
-pygame.display.update()
+    WIDTH = 800
+    HEIGHT = 480
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-# walls
-wcolor = pygame.Color("green")
-borderwidth = 10
-# upper wall
-# Rect((left, top), (width, height))
-pygame.draw.rect(screen, wcolor, pygame.Rect((0, 0), (WIDTH, borderwidth)))
-# left wall
-pygame.draw.rect(screen, wcolor, pygame.Rect((0, 0), (borderwidth, HEIGHT)))
-# bottom wall
-pygame.draw.rect(screen, wcolor, pygame.Rect(
-    (0, HEIGHT-borderwidth), (WIDTH, borderwidth)))
+    pygame.display.update()
 
-pygame.display.update()
+    # walls
+    wcolor = pygame.Color("green")
+    borderwidth = 10
+    # upper wall
+    # Rect((left, top), (width, height))
+    pygame.draw.rect(screen, wcolor, pygame.Rect((0, 0), (WIDTH, borderwidth)))
+    # left wall
+    pygame.draw.rect(screen, wcolor, pygame.Rect((0, 0), (borderwidth, HEIGHT)))
+    # bottom wall
+    pygame.draw.rect(screen, wcolor, pygame.Rect(
+        (0, HEIGHT-borderwidth), (WIDTH, borderwidth)))
 
-running = True
-#  main loop
-while running:
-    # event handling, gets all event from the event queue
-    for event in pygame.event.get():
-        # only do something if the event is of type QUIT
-        if event.type == pygame.QUIT:
-            # change the value to False, to exit the main loop
-            running = False
+    pygame.display.update()
+
+    running = True
+    #  main loop
+    while running:
+        # event handling, gets all event from the event queue
+        for event in pygame.event.get():
+            # only do something if the event is of type QUIT
+            if event.type == pygame.QUIT:
+                # change the value to False, to exit the main loop
+                running = False
+                
+if __name__ == '__main__':
+    main()
